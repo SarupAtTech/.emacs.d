c9edc9922c0028303576c9e3b75884383979a248
Sarup <sarupattech@gmail.com>
2025-08-30 17:08:00 +0900

    Add early-init.el

---
+++ early-init.el
@@ -0,0 +1,9 @@
+;;; early-init.el --- early init.el                  -*- lexical-binding: t; -*-
+;;; LAST UPDATE : 2023/08/15 18:20:42
+;;; Commentary:
+
+;;; Code:
+
+(setq byte-compile-warnings '(cl-functions))
+(eval-when-compile (setq byte-compile-warnings '(cl-functions)))
+;;; early-init.el ends here
