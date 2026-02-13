63ea917e6d4f7748cc180126db168ec5606efa6a
Shoichiro Saruwatari <s.saruwatari@secure-cycle.co.jp>
2026-02-13 13:44:02 +0900

    add define-keys

--- inits-available/30-vterm.el
+++ inits-available/30-vterm.el
@@ -1,5 +1,5 @@
 ;;; 30-vterm.el --- Summary
-;;; LAST UPDATE : 2025/12/12 01:14:25
+;;; LAST UPDATE : 2026/01/23 10:58:33
 ;;; Commentary:
 
 ;;; Code:
@@ -157,6 +157,14 @@ Installs a process sentinel and a buffer-local kill-buffer-hook."
 
 ;; Enable for all vterm buffers by default
 (add-hook 'vterm-mode-hook #'my/vterm-enable-auto-save)
+(add-hook 'vterm-mode-hook
+          #'(lambda ()
+              (define-key vterm-mode-map [(C t)] 'other-window-horizontally)
+              (define-key vterm-mode-map [(M t)] 'other-window-vertically)
+              (define-key vterm-mode-map [(M q)] 'delete-window)
+              (define-key vterm-mode-map [(C h)] 'vterm-send-backspace)
+              (define-key vterm-mode-map [(C u)] 'vterm--self-insert)
+          ))
 
 (provide 'my-vterm-autosave)
 
